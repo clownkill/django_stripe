669ffe465091de65ce935b35fb7a00be4a99b168
Evgeniy Alehnovich <evgeniy_alehnovich@proton.me>
2023-02-14 22:58:04 +0300

    add models for order

--- app/payments/admin.py
+++ app/payments/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Item
+from .models import Item, Tax, Discount, Order, OrderItem
 
 
 @admin.register(Item)
@@ -9,4 +9,55 @@ class ItemAdmin(admin.ModelAdmin):
         'name',
         'description',
         'price',
+    ]
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'amount',
+    ]
+
+
+@admin.register(Discount)
+class Discount(admin.ModelAdmin):
+    list_display = [
+        'code',
+        'amount',
+        'valid_from',
+        'valid_to',
+        'active',
+    ]
+    list_editable = [
+        'active',
+    ]
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = [
+        'product',
+    ]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'customer',
+        'created_at',
+        'updated_at',
+        'paid',
+    ]
+    list_filter = [
+        'paid',
+        'created_at',
+        'updated_at',
+    ]
+    raw_id_fields = [
+        'customer',
+    ]
+    inlines = [
+        OrderItemInline,
     ]
--- app/payments/models.py
+++ app/payments/models.py
@@ -1,11 +1,14 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import signals
 from django.dispatch import receiver
+from django.core.validators import MinValueValidator, MaxValueValidator
 import stripe
 
 
 class Item(models.Model):
+
     name = models.CharField('Название', max_length=150, db_index=True)
     description = models.TextField('Описание')
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
@@ -18,6 +21,117 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Tax(models.Model):
+
+    name = models.CharField('Название налога', max_length=150)
+    amount = models.PositiveSmallIntegerField(
+        'Величина налога',
+        validators=[MaxValueValidator(100)],
+    )
+
+    class Meta:
+        ordering = ('amount',)
+        verbose_name = 'Налог'
+        verbose_name_plural = 'Налоги'
+
+    def __str__(self):
+        return self.name
+
+
+class Discount(models.Model):
+
+    code = models.CharField(
+        'Купон',
+        max_length=50,
+        unique=True,
+    )
+    valid_from = models.DateTimeField('Действителен с')
+    valid_to = models.DateTimeField('Действителен по')
+    amount = models.PositiveSmallIntegerField(
+        'Величина скидки',
+        validators=[MaxValueValidator(100)]
+    )
+    active = models.BooleanField('Активный?')
+
+    class Meta:
+        verbose_name = 'Купон'
+        verbose_name_plural = 'Купоны'
+
+    def __str__(self):
+        return self.code
+
+
+class Order(models.Model):
+
+    customer = models.ForeignKey(
+        User,
+        verbose_name='Покупатель',
+        related_name='orders',
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+    tax = models.ForeignKey(
+        Tax,
+        verbose_name='Налог',
+        related_name='orders',
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    discount = models.ForeignKey(
+        Discount,
+        verbose_name='Скидка',
+        related_name='orders',
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created_at',)
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return f'Заказ {self.id}'
+
+    def get_total_cost(self):
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        if self.tax:
+            total_cost = total_cost + total_cost * self.tax.amount / 100
+        if self.discount:
+            total_cost = total_cost - total_cost * self.discount.amount / 100
+        return total_cost
+
+
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(
+        Order,
+        verbose_name='Заказ',
+        related_name='items',
+        on_delete=models.CASCADE,
+    )
+    product = models.ForeignKey(
+        Item,
+        verbose_name='Товар',
+        related_name='order_items',
+        on_delete=models.CASCADE,
+    )
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField('Количество', default=1)
+
+    class Meta:
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
 
 
 @receiver(signals.pre_save, sender=Item)
